4f8eb1b9df56139f2298d56511783648bd395b3c
Pablo Ruiz <pruizlezcano@gmail.com>
2020-10-13 15:45:45 +0200

    fix(translate): break line input

---
+++ commands/translate.js
@@ -0,0 +1,20 @@
+const translator = require('../utils/transaltor');
+module.exports = {
+  name: 'translate',
+  description: 'Translate text to a given language',
+  async execute(message, client, args) {
+    let text = '';
+    if (!message.quotedMsg) {
+      text = args
+        .reduce((prev, act) => (prev = prev + ' ' + act))
+        .split('\n')
+        .join(' ');
+    } else {
+      text = message.quotedMsg.body.split('\n').join(' ');
+    }
+    const to = args.shift();
+    console.log(text);
+    const translate = await translator(text, to);
+    await client.reply(message.from, translate, message.id);
+  },
+};
