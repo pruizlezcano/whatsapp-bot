576a3d640f9f2cbd219d4dac2af5e8b34a878d94
Pablo Ruiz <pruizlezcano@gmail.com>
2020-10-13 15:48:18 +0200

    feat(command): send a random joke or a programming joke

---
+++ commands/joke.js
@@ -0,0 +1,20 @@
+const axios = require('axios');
+module.exports = {
+  name: 'joke',
+  description: 'Send a joke',
+  async execute(message, client, args) {
+    let data = undefined;
+    if (args[0] === 'pr') {
+      const res = await axios.get(
+        'https://official-joke-api.appspot.com/jokes/programming/random'
+      );
+      data = res.data[0];
+    } else {
+      const res = await axios.get(
+        'https://official-joke-api.appspot.com/jokes/general/random'
+      );
+      data = res.data[0];
+    }
+    return client.sendText(message.chatId, `*${data.setup}*${data.punchline}`);
+  },
+};
