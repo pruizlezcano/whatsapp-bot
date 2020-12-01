7d2e96c4ad4feb4d353df4c7f187faca51e1d271
Pablo Ruiz <pruizlezcano@gmail.com>
2020-12-01 10:26:55 +0100

    fix(twitter): change package

--- commands/twitter.js
+++ commands/twitter.js
@@ -1,18 +1,19 @@
-const { twitter } = require('video-url-link');
+const youtubedl = require('youtube-dl');
 const shortener = require('../utils/shortener');
 
 const tweet = (url) =>
   new Promise((resolve, reject) => {
     console.log('Get metadata from =>', url);
-    twitter.getInfo(url, {}, (error, info) => {
-      if (error) {
-        console.error(error);
-        reject(error);
+    youtubedl.getInfo(url, [], function (err, info) {
+      if (err) {
+        console.error(err);
+        reject(err);
       } else {
         resolve(info);
       }
     });
   });
+
 module.exports = {
   name: 'twitter',
   description: 'Download video from Twitter',
@@ -24,6 +25,14 @@ module.exports = {
     } else {
       url = message.quotedMsg.canonicalUrl;
     }
+    if (url == undefined) {
+      const body = message.quotedMsg.body.split(' ');
+      for (i of body) {
+        if (i.indexOf('http') != -1) {
+          url = i;
+        }
+      }
+    }
     const isUrl = new RegExp(
       /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)/gi
     );
@@ -34,23 +43,15 @@ module.exports = {
       return client.reply(message.chatId, 'Invalid URL', message.id);
     await client.reply(message.from, `Loading...`, message.id);
 
-    const data = await tweet(url);
-    const info = data.variants[data.variants.length - 1];
-    if (info.content_type === 'video/mp4') {
-      const shortUrl = await shortener(info.url);
+    const info = await tweet(url);
+    const shortUrl = await shortener(info.url);
 
-      console.log('Video link: ' + shortUrl);
-      await client.sendFileFromUrl(
-        message.chatId,
-        info.url,
-        'twitter.mp4',
-        `Download link: ${shortUrl}`
-      );
-    } else {
-      client.sendText(
-        message.chatId,
-        `An error occurred. Maybe this tweet is not a video`
-      );
-    }
+    console.log('Video link: ' + shortUrl);
+    await client.sendFileFromUrl(
+      message.chatId,
+      info.url,
+      'twitter.mp4',
+      `Download link: ${shortUrl}`
+    );
   },
 };
